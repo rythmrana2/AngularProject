8d17c5a32aaed4ec0c6cbd2c48810b4defd38936
Rythm Rana <rythmrana2@gmail.com>
2022-07-13 07:31:31 +0530

    added functionality to display selected recipe in detail

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,11 +8,16 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg')
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'),
+    new Recipe('Test Recipe 2', 'This is simply another test', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg')
   ];
+  @Output() itemGotSelected = new EventEmitter<Recipe>();
   constructor() { }
 
   ngOnInit(): void {
+  }
+  selectedItem(selectedItem:Recipe){
+      this.itemGotSelected.emit(selectedItem);
   }
 
 }
